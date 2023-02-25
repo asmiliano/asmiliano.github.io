6cfc1d1eaa90f9b088feb1ca50e3215e11cfb3c5
Azat <102428810+asmiliano@users.noreply.github.com>
2023-02-25 23:31:53 +0600

    Update const.js

--- const.js
+++ const.js
@@ -1,56 +1,106 @@
+
 const movies = [
 	{
-	  title: "The Dark Knight",
-	  year: 2008,
-	  genre: "Action, Crime, Drama",
-	  rating: 9.0,
-	  image: "https://images.unsplash.com/photo-1611664229821-2f5c9a5aebf3"
+	  title: "Mr.Robot",
+	  year: 2015,
+	  genre: "Drama,Psychological-thriller",
+	  image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rottentomatoes.com%2Ftv%2Fmr_robot&psig=AOvVaw0HV5R8fWwThE0yPcYV5sbC&ust=1677383528138000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOiAg93ir_0CFQAAAAAdAAAAABAE",
 	},
 	{
-	  title: "Inception",
+	  title: "Social Network",
 	  year: 2010,
-	  genre: "Action, Adventure, Sci-Fi",
-	  rating: 8.8,
-	  image: "https://images.unsplash.com/photo-1619463703047-5c4a4ab1b202"
+	  genre: "Action,Spy,Thriller",
+	  image: "https://images.unsplash.com/photo-1619463703047-5c4a4ab1b202",
 	},
 	{
-	  title: "The Godfather",
-	  year: 1972,
-	  genre: "Crime, Drama",
-	  rating: 9.2,
-	  image: "https://images.unsplash.com/photo-1611272620026-7cb50a8da8fe"
+	  title: "Snowden",
+	  year: 2016,
+	  genre: "Biographic Drama",
+	  image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FM%2FMV5BMTg2MzYzNzgzOF5BMl5BanBnXkFtZTgwOTg4NzQ4OTE%40._V1_.jpg&imgrefurl=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt3774114%2F&tbnid=vWQBOWqFADPX6M&vet=12ahUKEwjxveyI7a_9AhWeBXcKHRR4CVMQMygAegUIARDlAQ..i&docid=CM0RB1cLHk1RjM&w=1899&h=2962&q=snowden%20movie&ved=2ahUKEwjxveyI7a_9AhWeBXcKHRR4CVMQMygAegUIARDlAQ",
 	},
   ];
   
-  const movieList = document.querySelector(".movie-list");
-  movies.forEach(movie => {
-	const movieElement = document.createElement("div");
-	movieElement.classList.add("movie");
-  
-	const imageElement = document.createElement("img");
-	imageElement.src = movie.image;
-	movieElement.appendChild(imageElement);
-  
-	const detailsElement = document.createElement("div");
-	detailsElement.classList.add("details");
-  
-	const titleElement = document.createElement("h3");
-	titleElement.textContent = movie.title;
-	detailsElement.appendChild(titleElement);
-  
-	const yearElement = document.createElement("p");
-	yearElement.textContent = `Year: ${movie.year}`;
-	detailsElement.appendChild(yearElement);
-  
-	const genreElement = document.createElement("p");
-	genreElement.textContent = `Genre: ${movie.genre}`;
-	detailsElement.appendChild(genreElement);
-  
-	const ratingElement = document.createElement("p");
-	ratingElement.textContent = `Rating: ${movie.rating}`;
-	detailsElement.appendChild(ratingElement);
-  
-	movieElement.appendChild(detailsElement);
-	movieList.appendChild(movieElement);
-  });
-  
+
+
+// for loop / forEach  
+// function {return const number} 
+// function (argument) { obejct (argument ) return object} 
+// show via console or html 
+
+const images1 = ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rottentomatoes.com%2Ftv%2Fmr_robot&psig=AOvVaw0HV5R8fWwThE0yPcYV5sbC&ust=1677383528138000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOiAg93ir_0CFQAAAAAdAAAAABAE"];
+const images2 = ["movie.png"];
+const images3 = [""];  
+for (let x = 0; x < images.length; x++) {
+	const element = 'img src= "' + images[0] + '"'
+	let pic = document.querySelector('.images').innerHTML = element;
+	pic;
+}
+
+function getObject(title,year,genre,image) {
+	let obejct = {
+		 title: title,
+		year: year,
+		genre: genre,
+		image: image,
+
+	}
+	return Object;
+
+}
+let object=getObject('Mr.Robot','2015','dramma','https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rottentomatoes.com%2Ftv%2Fmr_robot&psig=AOvVaw0HV5R8fWwThE0yPcYV5sbC&ust=1677383528138000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCOiAg93ir_0CFQAAAAAdAAAAABAE');
+const Aza=document.getElementById("movies")
+
+
+let userName = 'Azat';
+function showMessage () {
+	let message = 'Hi!' + userName;
+	alert(message);
+}
+showMessage ();
+
+
+let azaHTML="";
+object.forEach(moviie=>{
+	azaHTML+=`
+       <div class="movie">
+			<img src="${movie.image}">
+	   </div>
+	`;
+});
+Aza.innerHTML=azaHTML;
+
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
